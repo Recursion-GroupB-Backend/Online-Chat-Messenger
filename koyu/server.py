86226e135ffe0ace53c7f4f6700049a9893eec81
seiichi0404 <yamayamaseisei1834@gmail.com>
2023-12-01 20:28:14 +0900

    初期値の設定

--- koyu/server.py
+++ koyu/server.py
@@ -3,25 +3,33 @@ import threading
 import time
 
 class Server:
-    def __init__(self):
-        self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.server_sock.bind(('0.0.0.0', 9001))
+    TIME_OUT = 60
+    def __init__(self, tcp_address = "127.0.0.1", udp_address = "127.0.0.1"):
+        self.tcp_address = tcp_address
+        self.udp_address = udp_address
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        self.tcp_socket.bind((self.tcp_address, 9000))
+        self.udp_socket.bind((self.udp_address, 9001))
         self.clients = {}
+        self.rooms = {}
         self.TIMEOUT = 60
 
     def start(self):
         # 同時にメッセージ受信とクライアントのタイムアウト監視を行うことで効率的な並行処理が可能になる。
-        thread_check_timeout    = threading.Thread(target=self.check_client_timeout, daemon=True)
+        thread_check_timeout = threading.Thread(target=self.check_client_timeout, daemon=True)
         thread_check_timeout.start()
-        thread_recieve_message  = threading.Thread(target=self.recieve_message, daemon=True)
+        thread_recieve_message = threading.Thread(target=self.recieve_message, daemon=True)
         thread_recieve_message.start()
+
     def recieve_message(self):
         try:
             while True:
                 print('\nwaiting to receive message')
                 # データの取得（データを受け取るまで処理は止まる）
-                data, address = self.server_sock.recvfrom(4096)
-                
+                data, address = self.udp_socket.recvfrom(4096)
+
                 # 取得データを適切に処理
                 username_len = int.from_bytes(data[:1], byteorder='big')
                 username = data[1:1 + username_len].decode('utf-8')
@@ -32,10 +40,10 @@ class Server:
 
                 # 接続されている全てのクライアントにメッセージを送信
                 self.broadcast(message_for_send.encode('utf-8'), address)
-        
+
         finally:
             print('socket closig....')
-            self.server_sock.close()
+            self.udp_socket.close()
 
     def broadcast(self, message:bytes, self_address=None):
         for address in self.clients:
@@ -43,8 +51,8 @@ class Server:
                 # クライアントが自分自身で送信したメッセージは本人には返さない。
                 pass
             else:
-                self.server_sock.sendto(message, address)
-            
+                self.udp_socket.sendto(message, address)
+
     def check_client_timeout(self):
         try:
             while True:
@@ -59,15 +67,15 @@ class Server:
                 time.sleep(10)  # 10秒ごとに監視
         finally:
             print('socket closig....')
-            self.server_sock.close()
-    
+            self.udp_socket.close()
+
     def shutdown(self):
         print("Server is shutting down.")
-        self.server_sock.close()
+        self.udp_socket.close()
         # その他のクリーンアップ処理があればここに追加
-    
+
 if __name__ == "__main__":
-    server = Server()
+    server = Server("0.0.0.0", "0.0.0.0")
     server.start()
     try:
         while True:  # メインスレッドをアクティブに保つ
